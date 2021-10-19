6e5c145a56ec804ac846dce79e24effd15d663b7
ju851bah <julian@bahner.de>
2021-10-19 22:26:20 +0200

    Added initial backend

---
+++ backend/src/main/java/de/htwg/aume/AumeApplication.java
@@ -0,0 +1,13 @@
+package de.htwg.aume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AumeApplication.class, args);
+	}
+
+}
