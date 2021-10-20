c179d80fb83c2c8a251f61a3c0e7e44b6c801f0d
maxiflet <maximilian.fletschinger@gmail.com>
2021-10-20 20:53:36 +0200

    implement first version of session page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import LandingPage from '../views/LandingPage.vue';
+import SessionPage from '../views/SessionPage.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'LandingPage',
     component: LandingPage,
+  },
+  {
+    path: '/session',
+    name: 'SessionPage',
+    component: SessionPage,
   },
   {
     path: '/waitingRoom',
