73cd68f550d7911b277d02f9575d62f9133b2684
ju851bah <julian@bahner.de>
2021-10-20 16:44:11 +0200

    Added Mongo repository for session

--- backend/src/main/java/de/htwg/aume/AumeApplication.java
+++ backend/src/main/java/de/htwg/aume/AumeApplication.java
@@ -2,8 +2,10 @@ package de.htwg.aume;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class AumeApplication {
 
 	public static void main(String[] args) {
