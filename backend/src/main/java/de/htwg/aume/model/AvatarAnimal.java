2c7e19dfab6c1840004e8c57fbfe2cbc56fa7e21
julian <julian@formigas.de>
2021-10-22 11:34:00 +0200

    Added join route

---
+++ backend/src/main/java/de/htwg/aume/model/AvatarAnimal.java
@@ -0,0 +1,7 @@
+package de.htwg.aume.model;
+
+public enum AvatarAnimal {
+    WOLF, ZEBRA, PIGEON, DOG, CAMEL, HORSE, RABBIT, ELEPHANT, GOOSE, CAT, GIRAFFE, LION, MONKEY, DUCK, FISH, MOUSE,
+    TURTLE, TIGER, BULL, COW
+
+}
