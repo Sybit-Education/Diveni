72710702c0a313f7953d9521b74ebabe315b06da
julian <julian@formigas.de>
2021-10-24 00:30:29 +0200

    Added Principals in backend

---
+++ backend/src/main/java/de/htwg/aume/principals/AdminPrincipal.java
@@ -0,0 +1,24 @@
+package de.htwg.aume.principals;
+
+import java.security.Principal;
+import java.util.UUID;
+
+public class AdminPrincipal implements Principal {
+    private final UUID sessionID;
+    private final UUID adminID;
+
+    public AdminPrincipal(UUID sessionID, UUID adminID) {
+        this.adminID = adminID;
+        this.sessionID = sessionID;
+    }
+
+    @Override
+    public String getName() {
+        return adminID.toString();
+    }
+
+    public UUID getSessionID() {
+        return sessionID;
+    }
+
+}
---
+++ backend/src/main/java/de/htwg/aume/principals/MemberPrincipal.java
@@ -0,0 +1,24 @@
+package de.htwg.aume.principals;
+
+import java.security.Principal;
+import java.util.UUID;
+
+public class MemberPrincipal implements Principal {
+    private final UUID sessionID;
+    private final UUID memberID;
+
+    public MemberPrincipal(UUID sessionID, UUID memberID) {
+        this.memberID = memberID;
+        this.sessionID = sessionID;
+    }
+
+    @Override
+    public String getName() {
+        return memberID.toString();
+    }
+
+    public UUID getSessionID() {
+        return sessionID;
+    }
+
+}
