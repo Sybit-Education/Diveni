cd625284dcd2be950727e1d1a680252e6a9f46c2
julian <julian@formigas.de>
2021-10-26 15:25:02 +0200

    Replaced model classes with interfaces

--- frontend/src/model/Member.ts
+++ frontend/src/model/Member.ts
@@ -1,23 +1,9 @@
-import UUID from './UUID';
-
-export default class Member {
-    memberID: UUID;
-
+interface Member {
+    memberID: string;
     name: string;
-
     hexColor: string;
-
     avatarAnimalAssetName: string;
-
     currentEstimation: number | null;
-
-    constructor(memberID: UUID, name:string, hexColor: string,
-      avatarAnimalAssetName: string,
-      currentEstimation: number|null) {
-      this.memberID = memberID;
-      this.name = name;
-      this.hexColor = hexColor;
-      this.avatarAnimalAssetName = avatarAnimalAssetName;
-      this.currentEstimation = currentEstimation;
-    }
 }
+
+export default Member;
