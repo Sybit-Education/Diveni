06b55331127372d000c5a9627aa45b7f3ae305a0
julian <julian@formigas.de>
2021-11-02 00:40:04 +0100

    Added build-production script

---
+++ build-production.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd frontend
+npm run build
+sed -i -e 's#<title>frontend</title>#<title>frontend</title><script src="https://unpkg.com/@egjs/flicking@4.0.0-beta.4/dist/flicking.pkgd.min.js"></script><link rel="stylesheet" href="https://unpkg.com/@egjs/flicking@4.0.0/dist/flicking.css">#g' ./dist/index.html
+cd ../backend
+mvn package -DskipTests
+cd ..
+docker compose build
+docker compose up
+
