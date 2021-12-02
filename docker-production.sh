699a0241e7a1bfc04ce2c14dd2c2e288548226f4
Julian Bahner <julian@bahner.de>
2021-12-02 13:28:51 +0100

    Changed image names and nginx port

--- docker-production.sh
+++ docker-production.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker save -o docker-deploy/backend.tar aume2021-team-2_backend
+docker save -o docker-deploy/backend.tar  aume2021-team-2_backend
 docker save -o docker-deploy/frontend.tar aume2021-team-2_frontend
 docker save -o docker-deploy/database.tar aume2021-team-2_database
 docker save -o docker-deploy/proxy.tar aume2021-team-2_proxy
