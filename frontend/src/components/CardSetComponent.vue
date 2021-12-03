1d5000f48820419ddecf639fe59360d8ff6eeca0
Julian Zellner <ju851zel@htwg-konstanz.de>
2021-12-03 10:47:02 +0100

    Added 7 and 9 as numbers in hours set

--- frontend/src/components/CardSetComponent.vue
+++ frontend/src/components/CardSetComponent.vue
@@ -63,8 +63,8 @@ export default Vue.extend({
         {
           name: 'Hours',
           description: 'Simply hours. Great for sub tasks.',
-          values: ['1', '2', '3', '4', '5', '6', '8', '10', '12', '16'],
-          activeValues: ['1', '2', '3', '4', '5', '6', '8'],
+          values: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '12', '16'],
+          activeValues: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
         },
       ],
     };
