3bff1f14563302d2b6de8c3c698842adf05c2766
DoVllasa <57847484+DoVllasa@users.noreply.github.com>
2022-01-13 20:06:52 +0100

    feat(frontend): added `UI Tabs` in prepare session (#67)

---
+++ frontend/src/components/StroyPointsComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h5>Planning without User Stories</h5>
+    <ul>
+      <li>You will vote with your selected cards</li>
+      <li>You will not be able to add User Stories once you've started the session</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "StoryPointsComponent",
+});
+</script>
---
+++ frontend/src/components/UserStoryComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h5>Integrate User Stories in your Poker-Planning</h5>
+    <ul>
+      <li>Write your User Stories below</li>
+      <li>Add title and description to each User Story</li>
+      <li>Use the CSV-Import to integrate User Stories in your session</li>
+      <li>You will be able to add and edit User Stories once you've started the session</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "UserStoryComponent",
+});
+</script>
