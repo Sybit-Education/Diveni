2b5681bac463cda5a5a1d11cc6cc11d0d5431662
dominimvllasa <do571vll@htwg-konstanz.de>
2022-01-13 20:05:19 +0100

    changed text in user story tab

--- frontend/src/components/UserStoryComponent.vue
+++ frontend/src/components/UserStoryComponent.vue
@@ -5,7 +5,7 @@
       <li>Write your User Stories below</li>
       <li>Add title and description to each User Story</li>
       <li>Use the CSV-Import to integrate User Stories in your session</li>
-      <li>You will not be able to add User Stories once you've started the session</li>
+      <li>You will be able to add and edit User Stories once you've started the session</li>
     </ul>
   </div>
 </template>
