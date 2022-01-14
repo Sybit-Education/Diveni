72b63461ccc2c512bb95895add70126a5af0f23c
Robin Genz <mail@robingenz.dev>
2022-01-14 09:49:44 +0100

    feat(frontend): i18n dropdown (#73)

---
+++ frontend/src/components/LocaleDropdown.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <b-dropdown :text="locales[$i18n.locale]" class="mt-4">
+      <b-dropdown-item
+        v-for="(locale, key) in locales"
+        :key="key"
+        :active="$i18n.locale === key ? true : false"
+        @click="setLocale(key)"
+        >{{ locale }}</b-dropdown-item
+      >
+    </b-dropdown>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "LocaleDropdown",
+  data() {
+    return {
+      locales: {
+        de: "German",
+        en: "Englisch",
+      },
+    };
+  },
+  methods: {
+    setLocale(locale) {
+      this.$i18n.locale = locale;
+      localStorage.setItem("locale", locale);
+    },
+  },
+});
+</script>
