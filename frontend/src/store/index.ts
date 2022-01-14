8287cd5c4191cb1733b1634d0992a4a70e84f991
Robin Genz <mail@robingenz.dev>
2022-01-14 13:14:19 +0100

    refactor(frontend): add store state typing (#75)

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,3 +1,4 @@
+import { StoreState } from "@/types";
 import SockJS from "sockjs-client";
 import Vue from "vue";
 import Vuex from "vuex";
@@ -6,7 +7,7 @@ import Constants from "../constants";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+export default new Vuex.Store<StoreState>({
   state: {
     stompClient: undefined,
     webSocketConnected: false,
@@ -32,23 +33,23 @@ export default new Vuex.Store({
       );
     },
     subscribeOnBackendWSMemberUpdates(state) {
-      state.stompClient.subscribe(Constants.webSocketMemberListenRoute, (frame) => {
+      state.stompClient?.subscribe(Constants.webSocketMemberListenRoute, (frame) => {
         state.memberUpdates = state.memberUpdates.concat([frame.body]);
       });
     },
     subscribeOnBackendWSStoriesUpdated(state) {
-      state.stompClient.subscribe(Constants.webSocketMemberListenUserStoriesRoute, (frame) => {
+      state.stompClient?.subscribe(Constants.webSocketMemberListenUserStoriesRoute, (frame) => {
         state.userStories = JSON.parse(frame.body);
       });
     },
     subscribeOnBackendWSAdminUpdate(state) {
-      state.stompClient.subscribe(Constants.webSocketMembersUpdatedRoute, (frame) => {
+      state.stompClient?.subscribe(Constants.webSocketMembersUpdatedRoute, (frame) => {
         console.log(`web socket admin receive update: message ${frame}`);
         state.members = JSON.parse(frame.body);
       });
     },
     sendViaBackendWS(state, { endPoint, data }) {
-      state.stompClient.send(endPoint, data);
+      state.stompClient?.send(endPoint, data);
     },
     clearStore(state) {
       state.members = [];
