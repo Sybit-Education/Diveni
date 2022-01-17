5d8bf367150379e51264eb8de94940289f8b59af
NPolster <43877997+NPolster@users.noreply.github.com>
2022-01-17 16:00:27 +0100

    Feature/aume 175 scheduled task (#83)
    * feat(backend): added scheduled task to delete old sessions
    
    * added tests
    
    * commit badge
    
    * commit badge
    
    Co-authored-by: Niklas Polster <niklas.polster@pedepe.de>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: jbahner <julian@bahner.de>

--- backend/src/main/java/de/htwg/aume/AumeApplication.java
+++ backend/src/main/java/de/htwg/aume/AumeApplication.java
@@ -3,9 +3,11 @@ package de.htwg.aume;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableScheduling
 public class AumeApplication {
 
 	public static void main(String[] args) {
