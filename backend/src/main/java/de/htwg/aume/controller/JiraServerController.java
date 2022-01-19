707ef5ec8cf3381e04b513729cfd26645d4b7b23
Niklas Polster <niklas.polster@pedepe.de>
2022-01-19 12:45:45 +0100

    exception handling

--- backend/src/main/java/de/htwg/aume/controller/JiraServerController.java
+++ backend/src/main/java/de/htwg/aume/controller/JiraServerController.java
@@ -1,35 +1,16 @@
 package de.htwg.aume.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import de.htwg.aume.Utils;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import de.htwg.aume.model.JiraRequestToken;
-import de.htwg.aume.model.JoinInfo;
-import de.htwg.aume.model.Member;
-import de.htwg.aume.model.Session;
-import de.htwg.aume.model.SessionConfig;
-import de.htwg.aume.model.SessionState;
 import de.htwg.aume.model.VerificationCode;
 import de.htwg.aume.service.DatabaseService;
 import de.htwg.aume.service.jira.JiraServerService;
@@ -47,12 +28,11 @@ public class JiraServerController {
 
 	@GetMapping(value = "/jira/oauth1/requestToken")
 	public ResponseEntity<JiraRequestToken> getRequestToken() {
-		
 		return new ResponseEntity<>(jiraServerService.getRequestToken(), HttpStatus.OK);
 	}
 
 	@PostMapping(value = "/jira/oauth1/verificationCode")
-	public ResponseEntity<String> getAccessToken(@RequestBody VerificationCode verificationCode) {
-		return new ResponseEntity<>(jiraServerService.getAccessToken(verificationCode.getCode(), verificationCode.getToken()), HttpStatus.OK);
+	public void getAccessToken(@RequestBody VerificationCode verificationCode) {
+		jiraServerService.getAccessToken(verificationCode.getCode(), verificationCode.getToken());
 	}
 }
--- backend/src/main/java/de/htwg/aume/service/jira/JiraServerService.java
+++ backend/src/main/java/de/htwg/aume/service/jira/JiraServerService.java
@@ -2,15 +2,19 @@ package de.htwg.aume.service.jira;
 
 
 import com.google.api.client.auth.oauth.OAuthAuthorizeTemporaryTokenUrl;
-import org.springframework.stereotype.Service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import de.htwg.aume.controller.ErrorMessages;
 import de.htwg.aume.model.JiraRequestToken;
 
 
 @Service
 public class JiraServerService {
     private final String JIRA_HOME = "http://5.189.185.210";
-    private final String CONSUMER_KEY = "OauthKey";
+    private final String CONSUMER_KEY = "OauthKeyd";
     private final String PRIVATE_KEY = "MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBAOMj4i7WeH+0OYkgr7+fhc2qJbdaOUh4JmnHBnVqQMwpZDotsyr7HCWZXlvTjbQCr2NQxq424yEbFitcK1ZuNfS8zTp8vm4Ch4u/FTQBm5nPP3fM4FkLYzZQmQqnStirzUFrGSK4jtqS5wh+lfgXCPOp3mSoDp7KVizHSBQTBtXbAgMBAAECgYEAqx83og3WTm+bASJtBbLK/Xz4WUBR87UBS6Ozy/W2x5lPdz1CxFjWhcUb/5ZMJZf4RpxucoXLa/+aHiScSctSPZo7GgOUL1rEVi/POdht8w67N/KWA57VQy9n087MaMCOWf3ezfxcUMv4fzolgF3TxIMM+cYnFULydl8yzMGnBRECQQDzw1m1al/qYCfBtUeuQVd98i+QPLRvFcTCvo0848NT/Cl81iUCymr2LY/dvJEx2xJ3eC4xPw1UlIwt9RN7JQpFAkEA7oromA0e3igxyeQ7zUBBEdZLk7RbrNZ7FJ1ShdRqMcD3Ln+nwkLr0g9o/2XGkh2+EexyKEk4P4iwrIXoddlxnwJAYQ5Q86itE/bBHaF+LuWZXm5FfdqNxQUX2KpiNfJB3XizVB83kUrjF63AcHsaHI2rZqIVUkpWlmym+81uukNfOQJBALZejKRyo37EzAvF6dJppVW1t+IcqVniQAbqoASg+O9Az7lE70SdVR0rmuJnNQDQrFeXpU8Xa2FnZ2r+lVJEA5ECQQC6fPxR9WBpaJM2hfEBDFxaggbTNwoPA3uOLOGSTYkHXBZ98idjesfu3ZOq06IsVaLZEuIz/hIvvrdzjYRnugFA";
             
 
@@ -31,20 +35,17 @@ public class JiraServerService {
             jiraRequestToken.setUrl(authorizationURL.toString());
             return jiraRequestToken;
         } catch (Exception e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ErrorMessages.failedToRetrieveRequestTokenErrorMessage);
         }
-
-        return null;
     }
 
-    public String getAccessToken(String verificationCode, String requestToken){
+    public void getAccessToken(String verificationCode, String requestToken){
         try {
             JiraOAuthClient jiraOAuthClient = new JiraOAuthClient(JIRA_HOME);
             String accessToken = jiraOAuthClient.getAccessToken(requestToken, verificationCode, CONSUMER_KEY, PRIVATE_KEY);
-            return accessToken;
         } catch (Exception e) {
-            return "";
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ErrorMessages.failedToRetrieveAccessTokenErrorMessage);
         }
     }
 
