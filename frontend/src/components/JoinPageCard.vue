815a1d5cf4dc1dab9153c5a7e02f3b4e4cd7a1e6
jbahner <julian@bahner.de>
2022-01-21 16:13:51 +0100

    Set focus on name field of joinpage

--- frontend/src/components/JoinPageCard.vue
+++ frontend/src/components/JoinPageCard.vue
@@ -17,7 +17,7 @@
       <b-row class="mt-2">
         <b-col>
           <h6>{{ $t("page.join.input.name") }}</h6>
-          <b-form-input v-model="name" class="mt-3" type="text" />
+          <b-form-input ref="name" v-model="name" class="mt-3" type="text" />
         </b-col>
       </b-row>
       <b-row class="mt-4">
@@ -26,13 +26,8 @@
           <b-form-input v-model="sessionID" class="mt-3" type="text" />
         </b-col>
         <b-col class="mt-2 mt-md-0" cols="12" md="6">
-          <h6>{{ $t("page.join.input.password") }}</h6>
-          <b-form-input
-            v-model="password"
-            class="mt-3"
-            type="password"
-            :placeholder="$t('page.join.input.passwordPlaceholder')"
-          />
+          <h6>Password</h6>
+          <b-form-input v-model="password" class="mt-3" type="password" placeholder="(optional)" />
         </b-col>
       </b-row>
       <b-row>
@@ -84,6 +79,9 @@ export default Vue.extend({
         this.onClickButton();
       }
     });
+  },
+  mounted() {
+    this?.$refs?.name?.["$el"]?.focus();
   },
   methods: {
     onClickButton() {
