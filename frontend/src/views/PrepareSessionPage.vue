98cc30700f92e3e4c7ea7758daef7ed5e2b81de0
Jan296929B <jan.berchtld@htwg-konstanz.de>
2022-01-22 19:49:36 +0100

    change text for create user story tab

--- frontend/src/views/PrepareSessionPage.vue
+++ frontend/src/views/PrepareSessionPage.vue
@@ -10,21 +10,13 @@
       <b-tabs v-model="tabIndex" content-class="mt-3" fill>
         <b-tab
           class="mg_top_2_per"
-          :title="
-            $t(
-              'session.prepare.step.selection.mode.description.withoutUS.tab.label'
-            )
-          "
+          :title="$t('session.prepare.step.selection.mode.description.withoutUS.tab.label')"
           :title-link-class="linkClass(0)"
         >
           <stroy-points-component />
         </b-tab>
         <b-tab
-          :title="
-            $t(
-              'session.prepare.step.selection.mode.description.withUS.tab.label'
-            )
-          "
+          :title="$t('session.prepare.step.selection.mode.description.withUS.tab.label')"
           :title-link-class="linkClass(1)"
         >
           <user-story-component class="mg_top_2_per" />
@@ -35,26 +27,13 @@
             accept="text/csv"
             @change="importStory($event.target.files)"
           />
-          <b-button
-            block
-            color="primary"
-            elevation="2"
-            @click="openFileUploader()"
-          >
-            {{
-              $t(
-                "session.prepare.step.selection.mode.description.withUS.importButton"
-              )
-            }}
+          <b-button block color="primary" elevation="2" @click="openFileUploader()">
+            {{ $t("session.prepare.step.selection.mode.description.withUS.importButton") }}
           </b-button>
         </b-tab>
         <b-tab
           v-if="isJiraEnabled"
-          :title="
-            $t(
-              'session.prepare.step.selection.mode.description.withJira.tab.label'
-            )
-          "
+          :title="$t('session.prepare.step.selection.mode.description.withJira.tab.label')"
           :title-link-class="linkClass(2)"
         >
           <jira-component class="mg_top_2_per" />
@@ -63,18 +42,13 @@
       <h4 class="mt-4">
         {{ $t("session.prepare.step.selection.cardSet.title") }}
       </h4>
-      <card-set-component
-        class="mt-3"
-        @selectedCardSetOptions="setCardSetOptions"
-      />
+      <card-set-component class="mt-3" @selectedCardSetOptions="setCardSetOptions" />
       <h4 class="mt-3">
         {{ $t("session.prepare.step.selection.time.title") }}
       </h4>
       <b-row class="mt-3 text-center">
         <b-col>
-          <b-button variant="outline-secondary" @click="setTimerDown()">
-            -</b-button
-          >
+          <b-button variant="outline-secondary" @click="setTimerDown()"> -</b-button>
         </b-col>
         <b-col class="text-center">
           <h4>
@@ -82,9 +56,7 @@
           </h4>
         </b-col>
         <b-col>
-          <b-button variant="outline-secondary" @click="setTimerUp()">
-            +</b-button
-          >
+          <b-button variant="outline-secondary" @click="setTimerUp()"> +</b-button>
         </b-col>
       </b-row>
       <h4 class="mt-3">
@@ -97,9 +69,7 @@
               <b-form-input
                 id="input-password"
                 v-model="password"
-                :placeholder="
-                  $t('session.prepare.step.selection.password.placeholder')
-                "
+                :placeholder="$t('session.prepare.step.selection.password.placeholder')"
               />
             </b-form-group>
           </b-form>
@@ -273,12 +243,8 @@ export default Vue.extend({
 
           file.data.forEach((story) => {
             let title = story.title ? story.title : story.Title;
-            let description = story.description
-              ? story.description
-              : story.Description;
-            let estimation = story.estimation
-              ? story.estimation
-              : story.Estimation;
+            let description = story.description ? story.description : story.Description;
+            let estimation = story.estimation ? story.estimation : story.Estimation;
 
             stories.push({
               title: title,
@@ -298,9 +264,7 @@ export default Vue.extend({
         },
         error: function (err, file, inputElem, reason) {
           this.$toast.error(
-            this.$t(
-              "session.prepare.step.selection.mode.description.withUS.toastErrorNotification"
-            )
+            this.$t("session.prepare.step.selection.mode.description.withUS.toastErrorNotification")
           );
         },
       });
