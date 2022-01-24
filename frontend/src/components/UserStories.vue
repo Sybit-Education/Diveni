ea62272a0b9b917abedc8d7b1caf8e19163e5a89
Jan296929B <jan.berchtld@htwg-konstanz.de>
2022-01-24 17:07:35 +0100

    change padding

--- frontend/src/components/UserStories.vue
+++ frontend/src/components/UserStories.vue
@@ -16,7 +16,6 @@
       <div class="list-group list-group-horizontal">
         <b-button
           v-if="showEditButtons"
-          class="mx-1"
           :variant="story.isActive ? 'success' : 'outline-success'"
           @click="markUserStory(index)"
         >
@@ -25,12 +24,15 @@
         <b-form-input
           v-model="userStories[index].title"
           :disabled="true"
-          class="border-1"
+          class="border-1 mx-1"
           size="lg"
           :style="{
-            'background-color': index === number ? 'RGB(202, 202, 202)' : 'white',
+            'background-color':
+              index === number ? 'RGB(202, 202, 202)' : 'white',
           }"
-          :placeholder="$t('page.session.before.userStories.placeholder.userStoryTitle')"
+          :placeholder="
+            $t('page.session.before.userStories.placeholder.userStoryTitle')
+          "
           @blur="publishChanges"
         />
         <b-button
@@ -47,7 +49,9 @@
             class="card-body rounded"
             :style="{
               'background-color':
-                userStories[index].estimation == null ? 'white' : 'RGB(13, 202, 240)',
+                userStories[index].estimation == null
+                  ? 'white'
+                  : 'RGB(13, 202, 240)',
               width: '48px',
               'font-size': 'larger',
             }"
@@ -66,7 +70,12 @@
       {{ $t("page.session.before.userStories.button.addFirstUserStory") }}
     </b-button>
     <div v-if="userStories.length > 0 && showEditButtons">
-      <b-button class="w-100 h-100" size="lg" variant="success" @click="addUserStory()">
+      <b-button
+        class="w-100 h-100"
+        size="lg"
+        variant="success"
+        @click="addUserStory()"
+      >
         {{ $t("page.session.before.userStories.button.addUserStory") }}
         <b-icon-plus />
       </b-button>
