a321c9cdd3f797b8a73fdfd06fbac74f6c860ef3
maxiflet <maximilian.fletschinger@gmail.com>
2022-01-24 21:04:07 +0100

    added sum of user stories for admin and members

---
+++ frontend/src/components/UserStorySum.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-row
+      v-if="userStorySum > 0"
+      align-h="between"
+    >
+      <b-col>
+        <h4 class="float-start">{{ $t("page.session.before.userStories.sum") }}</h4>
+        <h4 class="float-end me-4">{{ userStorySum }}</h4>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "UserStorySumComponent",
+  computed: {
+    userStorySum() {
+      var sum = 0;
+      this.$store.state.userStories.forEach((userStory) => {
+        if(userStory.estimation !== null) {
+          sum += parseInt(userStory.estimation);
+        }
+      });
+      return sum;
+    },
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/MemberVotePage.vue
+++ frontend/src/views/MemberVotePage.vue
@@ -107,6 +107,16 @@
         :highlight="highlightedMembers.includes(member.memberID) || highlightedMembers.length === 0"
       />
     </b-row>
+    <b-row
+      v-if="userStoryMode !== 'NO_US'"
+      class="mt-5"
+    >
+      <b-col md="6">
+        <UserStorySumComponent
+          class="ms-4"
+        />
+      </b-col>
+    </b-row>
     <b-row v-if="userStoryMode !== 'NO_US' && !isMobile">
       <b-col class="mt-2">
         <div class="overflow-auto" style="height: 700px">
@@ -159,6 +169,7 @@ import UserStories from "../components/UserStories.vue";
 import UserStoryDescriptions from "../components/UserStoryDescriptions.vue";
 import MobileStoryList from "../components/MobileStoryList.vue";
 import MobileStoryTitle from "../components/MobileStoryTitle.vue";
+import UserStorySumComponent from "@/components/UserStorySum.vue";
 
 export default Vue.extend({
   name: "MemberVotePage",
@@ -172,6 +183,7 @@ export default Vue.extend({
     UserStoryDescriptions,
     MobileStoryList,
     MobileStoryTitle,
+    UserStorySumComponent,
   },
   props: {
     memberID: { type: String, default: undefined },
