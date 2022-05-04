4595cc5e42b3ac6aa84cc0cacf766f2b20b7e9e6
KzuDemEvin <65013155+KzuDemEvin@users.noreply.github.com>
2022-05-04 13:27:51 +0200

    Feature/165 enhance docker deployment (#167)
    * Enhance deployment process
    
    * Remove unnecessary files
    
    * Update package-lock.json
    
    * Update README.md
    
    Co-authored-by: Schoch, Kevin <Kevin.Schoch@sybit.de>

--- docker-production.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-docker save -o docker-deploy/backend.tar  diveni_backend
-docker save -o docker-deploy/frontend.tar diveni_frontend
-docker save -o docker-deploy/database.tar diveni_database
-docker save -o docker-deploy/proxy.tar diveni_proxy
-
-tar -czf diveni.tar.gz -C docker-deploy .
-
