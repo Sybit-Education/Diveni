963813941198131d2054931a8c469cf760933cce
Schoch, Kevin <Kevin.Schoch@sybit.de>
2022-06-15 13:39:44 +0200

    Fix cors config

--- backend/src/main/java/io/diveni/backend/config/CorsConfig.java
+++ backend/src/main/java/io/diveni/backend/config/CorsConfig.java
@@ -1,20 +1,24 @@
 package io.diveni.backend.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
   @Value("${SERVER_URL:#{null}}")
   private String SERVER_URL;
 
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOrigins(SERVER_URL).allowedMethods("GET", "POST");
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins(SERVER_URL);
+      }
+    };
   }
 }
