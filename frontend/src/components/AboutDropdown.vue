6ece84209cee01adaad36fdb005a9c40ae17c5f3
Stephan Strittmatter <stephan@st-strittmatter.name>
2022-08-30 15:05:59 +0200

    update Links in about menu

--- frontend/src/components/AboutDropdown.vue
+++ frontend/src/components/AboutDropdown.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
-    <b-dropdown :text="$t('general.about')" class="mt-4 mb-2">
-      <b-dropdown-item @click="openFrontendLicenseTab()">{{
-        $t("general.licenses.frontend")
-      }}</b-dropdown-item>
-      <b-dropdown-item @click="openBackendLicenseTab()">{{
-        $t("general.licenses.backend")
-      }}</b-dropdown-item>
-    </b-dropdown>
-  </div>
+  <b-dropdown :text="$t('general.about.label')" class="mt-4 mb-2">
+    <b-dropdown-item href="https://github.com/Sybit-Education/Diveni">
+      {{ $t("general.about.repository") }}
+    </b-dropdown-item>
+    <b-dropdown-item href="https://docs.diveni.io">
+      {{ $t("general.about.documentation") }}
+    </b-dropdown-item>
+    <b-dropdown-divider></b-dropdown-divider>
+    <b-dropdown-item @click="openFrontendLicenseTab()">
+      {{ $t("general.licenses.frontend") }}
+    </b-dropdown-item>
+    <b-dropdown-item @click="openBackendLicenseTab()">
+      {{ $t("general.licenses.backend") }}
+    </b-dropdown-item>
+  </b-dropdown>
 </template>
 
 <script lang="ts">
