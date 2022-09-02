1bc3f5fabc739f658af8c4251238b0cc67eb5b12
Stephan Strittmatter <stephan@st-strittmatter.name>
2022-09-02 10:08:48 +0200

    chore(backend): add license headers to java test files

--- backend/src/test/java/io/diveni/backend/controller/RoutesControllerTest.java
+++ backend/src/test/java/io/diveni/backend/controller/RoutesControllerTest.java
@@ -1,3 +1,8 @@
+/*
+  SPDX-License-Identifier: AGPL-3.0-or-later
+  Diveni - The Planing-Poker App
+  Copyright (C) 2022 Diveni Team, AUME-Team 21/22, HTWG Konstanz
+*/
 package io.diveni.backend.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
