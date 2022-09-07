8b0ed5ea72784e07909cba6599874749121a8b5b
Stephan Strittmatter <stephan@st-strittmatter.name>
2022-09-07 10:43:52 +0200

    chore(frontend): add lib for markdown editor

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,26 +4,35 @@
   Copyright (C) 2022 AUME-Team 21/22, HTWG Konstanz
 */
 import Vue from "vue";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
 import axios from "axios";
 import VueAxios from "vue-axios";
-import VueFlicking from "@egjs/vue-flicking";
+
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import i18n from "./i18n";
+import setupInterceptors from "./interceptors";
+
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+
+import VueFlicking from "@egjs/vue-flicking";
+
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
-import setupInterceptors from "./interceptors";
+
+import VueMarkdownEditor from "@kangc/v-md-editor";
+import "@kangc/v-md-editor/lib/style/base-editor.css";
 
 Vue.use(VueAxios, axios);
 Vue.use(IconsPlugin);
 Vue.use(BootstrapVue);
 Vue.use(VueFlicking);
 Vue.use(Toast, {});
+Vue.use(VueMarkdownEditor);
 
 setupInterceptors();
 
