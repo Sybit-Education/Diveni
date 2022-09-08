ded924848056729aa14600d5e1ce2093e26e83e6
Stephan Strittmatter <stritti@users.noreply.github.com>
2022-09-08 14:56:26 +0200

    chore(frontend): refactore sessionLeveButton

--- frontend/src/components/actions/SessionLeaveButton.vue
+++ frontend/src/components/actions/SessionLeaveButton.vue
@@ -1,13 +1,26 @@
 <template>
-$END$
+  <b-button
+    v-b-modal.close-session-modal
+    style="max-height: 40px"
+    variant="danger"
+    class="mt-4"
+    @click="leaveSession"
+  >
+    <b-icon-x />
+    {{ $t("page.vote.button.leave.label") }}
+  </b-button>
 </template>
 
-<script>
-export default {
-name: "SessionLeaveButton"
-}
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "SessionLeaveButton",
+  methods: {
+    leaveSession() {
+      window.localStorage.removeItem("memberCookie");
+      this.$router.push({ name: "LandingPage" });
+    },
+  },
+});
 </script>
-
-<style scoped>
-
-</style>
