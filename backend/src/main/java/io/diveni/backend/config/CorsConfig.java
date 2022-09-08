ed294600b50be088dd6e24ed8c94dac1db956627
Stephan Strittmatter <stritti@users.noreply.github.com>
2022-09-08 11:44:44 +0200

    chore(backend): add logging for CORS information

--- backend/src/main/java/io/diveni/backend/config/CorsConfig.java
+++ backend/src/main/java/io/diveni/backend/config/CorsConfig.java
@@ -1,5 +1,8 @@
 package io.diveni.backend.config;
 
+import io.diveni.backend.controller.ProjectManagementController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +12,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(CorsConfig.class);
+
   @Value("${SERVER_URL:#{null}}")
-  private String SERVER_URL;
+  private static String SERVER_URL;
 
   @Bean
   public WebMvcConfigurer corsConfigurer() {
+    LOGGER.info("CORS: allow configured origin '{}'", SERVER_URL);
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
