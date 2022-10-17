c36f18c6b3bd3614cca079ba655eca8106a32435
Till Wanner <Till.Wanner@sybit.de>
2022-10-17 09:05:08 +0200

    228: add translated labels to the aboutPage

--- frontend/src/views/AboutPage.vue
+++ frontend/src/views/AboutPage.vue
@@ -58,7 +58,7 @@
         </a>
       </li>
       <li>
-        <a href="https://crowdin.com/project/diveni" target="_blank"> Translations </a>
+        <a href="https://crowdin.com/project/diveni" target="_blank"> {{ $t("general.licenses.translations") }} </a>
       </li>
     </ul>
   </b-container>
