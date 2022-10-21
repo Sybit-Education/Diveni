06283416af541f84e8254e713321786fefe462da
ravihidayat <naufalravi@graduate.utm.my>
2022-10-21 15:01:57 +0700

    Upgrade dependencies for the purpose of migration to Vue 3

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -14,4 +14,21 @@ module.exports = {
       msTileImage: "./favicon.ico",
     },
   },
+  chainWebpack: (config) => {
+    config.resolve.alias.set('vue', '@vue/compat')
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2
+            }
+          }
+        }
+      })
+  }
 };
