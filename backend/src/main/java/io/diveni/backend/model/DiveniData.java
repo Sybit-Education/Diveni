9e91c1fb523fc34c5412e474ac7d5184f9243255
Till Wanner <Till.Wanner@sybit.de>
2022-11-21 14:21:22 +0100

    forgot one file

---
+++ backend/src/main/java/io/diveni/backend/model/DiveniData.java
@@ -0,0 +1,19 @@
+package io.diveni.backend.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@EqualsAndHashCode
+@Document("data")
+public class DiveniData {
+
+    private final int amountOfAttendees;
+
+    private final int amountOfSessions;
+    
+}
