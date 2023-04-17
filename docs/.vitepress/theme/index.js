30f8b3287f1daab10d9a7a35a0eb0068e0a44e69
Schoch, Kevin <kevin.schoch@sybit.de>
2023-04-17 15:09:23 +0200

    feat: add back to top

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,8 @@
 import DefaultTheme from 'vitepress/theme'
 import { onMounted } from 'vue'
 import mediumZoom from 'medium-zoom'
+import vitepressBackToTop from 'vitepress-plugin-back-to-top'
+import 'vitepress-plugin-back-to-top/dist/style.css'
 
 import './custom.css'
 
@@ -10,5 +12,11 @@ export default {
     onMounted(() => {
       mediumZoom('.main img', { background: 'var(--vp-c-bg)' });
     })
-  }
+  },
+  enhanceApp({ app }) {
+    vitepressBackToTop({
+      // default
+      threshold:300
+    })
+  },
 };
