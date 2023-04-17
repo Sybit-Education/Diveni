523ce93f7a7ed834596f3118c294b603678e71cb
Schoch, Kevin <kevin.schoch@sybit.de>
2023-04-17 14:38:11 +0200

    feat: add image zoom

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,14 @@
 import DefaultTheme from 'vitepress/theme'
+import { onMounted } from 'vue'
+import mediumZoom from 'medium-zoom'
+
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme,
+  setup() {
+    onMounted(() => {
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' });
+    })
+  }
+};
