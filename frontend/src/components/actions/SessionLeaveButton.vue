e9fbb8a25509f708bc959ca703aa2b49d5f25714
SponsoredByPuma <92574150+SponsoredByPuma@users.noreply.github.com>
2023-04-28 12:11:30 +0200

    fixxed a bug where the session got closed and the users tried leaving a none existing session

--- frontend/src/components/actions/SessionLeaveButton.vue
+++ frontend/src/components/actions/SessionLeaveButton.vue
@@ -1,11 +1,5 @@
 <template>
-  <b-button
-    v-b-modal.close-session-modal
-    style="max-height: 40px"
-    variant="danger"
-    class="mt-4"
-    @click="leaveSession"
-  >
+  <b-button v-b-modal.close-session-modal style="max-height: 40px" variant="danger" class="mt-4" @click="leaveSession">
     <b-icon-x />
     {{ $t("page.vote.button.leave.label") }}
   </b-button>
@@ -13,11 +7,15 @@
 
 <script lang="ts">
 import Vue from "vue";
+import Constants from "../../constants";
 
 export default Vue.extend({
   name: "SessionLeaveButton",
   methods: {
     leaveSession() {
+      const endPoint = `${Constants.webSocketUnregisterRoute}`;
+      this.$store.commit("sendViaBackendWS", { endPoint, data: null });
+      this.$store.commit("clearStore");
       window.localStorage.removeItem("memberCookie");
       this.$router.push({ name: "LandingPage" });
     },
