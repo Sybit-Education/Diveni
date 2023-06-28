f4a1b8e3ab4f1024e22ddb7e63c28403d00e2e25
Schoch, Kevin <kevin.schoch@sybit.de>
2023-06-28 14:35:59 +0200

    fix: set mongodb version

--- backend/src/test/java/io/diveni/backend/controller/ConfigControllerTest.java
+++ backend/src/test/java/io/diveni/backend/controller/ConfigControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -15,7 +14,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
 public class ConfigControllerTest {
 
   @Autowired private MockMvc mockMvc;
