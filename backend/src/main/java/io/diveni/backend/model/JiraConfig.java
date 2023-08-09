b8f9d344b4386374ab8f0ffcfe8344887fe3b799
Schoch, Kevin <kevin.schoch@sybit.de>
2023-08-09 15:47:19 +0200

    fix: add NotNull to instance variables

--- backend/src/main/java/io/diveni/backend/model/JiraConfig.java
+++ backend/src/main/java/io/diveni/backend/model/JiraConfig.java
@@ -3,11 +3,21 @@ package io.diveni.backend.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @AllArgsConstructor
 public class JiraConfig {
+
+  @NotNull
   String jiraUrl;
+
+  @NotNull
   String accessToken;
+
+  @NotNull
   String consumerKey;
+
+  @NotNull
   String privateKey;
 }
