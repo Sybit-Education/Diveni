5a6bebf76a4bca3a4dc60dc29457a2d8c70bf2b6
SponsoredByPuma <tillwanner01@gmail.com>
2023-08-28 14:34:55 +0200

    missing imports

--- backend/src/main/java/io/diveni/backend/controller/WebsocketController.java
+++ backend/src/main/java/io/diveni/backend/controller/WebsocketController.java
@@ -22,9 +22,13 @@ import io.diveni.backend.service.WebSocketService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.text.ParseException;
 
 import io.diveni.backend.principals.AdminPrincipal;
 import io.diveni.backend.principals.MemberPrincipal;
