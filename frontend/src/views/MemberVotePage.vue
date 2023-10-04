ccbdc089713ea266e4e6c23069809c9100c46486
SponsoredByPuma <92574150+SponsoredByPuma@users.noreply.github.com>
2023-10-04 16:23:33 +0200

    #295 Timer depends on local time (#304)
    * Timer is no longer based on the Date time (System time)
    
    * websocket on main state
    
    * Google Java Format
    
    * add a solution to the timer which is based on local time
    
    * Google Java Format
    
    * missing imports
    
    * replaced created with ok, improved Logger Message
    
    ---------
    
    Co-authored-by: github-actions <>

--- frontend/src/views/MemberVotePage.vue
+++ frontend/src/views/MemberVotePage.vue
@@ -23,6 +23,7 @@
             :start-timestamp="timerTimestamp"
             :pause-timer="estimateFinished || pauseSession"
             :duration="timerCountdownNumber"
+            :member="memberID"
             :votingStarted="isStartVoting"
           />
         </b-col>
