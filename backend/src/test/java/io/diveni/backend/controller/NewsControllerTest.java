5c7e60984a1f3cd09735feb3dd4175c03890a94b
Valeri <valeri12580@gmail.com>
2023-10-04 11:57:19 +0300

    Add tests

--- backend/src/test/java/io/diveni/backend/controller/NewsControllerTest.java
+++ backend/src/test/java/io/diveni/backend/controller/NewsControllerTest.java
@@ -1,29 +1,52 @@
 package io.diveni.backend.controller;
 
+import io.diveni.backend.model.news.PullRequest;
+import io.diveni.backend.service.news.GithubApiService;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class NewsControllerTest {
 
+  private static final String path = "/news/pull-requests";
+
+  @Value("${vsc.github.api.pr.get}")
+  private String url;
+
   @Autowired
   private MockMvc mockMvc;
 
+  @MockBean
+  private RestTemplate client;
+
   @Test
   public void getPRs_InvalidState_returnsException() throws Exception {
-    MockHttpServletRequestBuilder requestBuilder = get("/news/pull-requests");
-    requestBuilder.param(NewsController.STATE_PARAM, "invalid");
+    String invalidState = "invalid";
+    MockHttpServletRequestBuilder requestBuilder = get(path);
+    requestBuilder.param(NewsController.STATE_PARAM, invalidState);
     requestBuilder.param(NewsController.PAGE_PARAM, "1");
     requestBuilder.param(NewsController.PER_PAGE_PARAM, "1");
     this.mockMvc.perform(requestBuilder)
@@ -34,7 +57,7 @@ public class NewsControllerTest {
 
   @Test
   public void getPRs_InvalidPerPage_returnsException() throws Exception {
-    MockHttpServletRequestBuilder requestBuilder = get("/news/pull-requests");
+    MockHttpServletRequestBuilder requestBuilder = get(path);
     requestBuilder.param(NewsController.STATE_PARAM, "closed");
     requestBuilder.param(NewsController.PAGE_PARAM, "1");
     requestBuilder.param(NewsController.PER_PAGE_PARAM, "5000");
@@ -47,15 +70,46 @@ public class NewsControllerTest {
 
   @Test
   public void getPRs_Valid_returnsPrs() throws Exception {
-    MockHttpServletRequestBuilder requestBuilder = get("/news/pull-requests");
+    PullRequest[] data = new PullRequest[]{
+      new PullRequest(403, "test2.com", "test2", LocalDate.now(), "user"),
+      new PullRequest(404, "test.com", "test", LocalDate.now(), "user")
+    };
+
+    when(client.exchange(contains(url), eq(HttpMethod.GET), any(), eq(PullRequest[].class)))
+      .thenReturn(ResponseEntity.ok(data.clone()));
+
+    MockHttpServletRequestBuilder requestBuilder = get(path);
     requestBuilder.param(NewsController.STATE_PARAM, "closed");
     requestBuilder.param(NewsController.PAGE_PARAM, "1");
     requestBuilder.param(NewsController.PER_PAGE_PARAM, "10");
+
     this.mockMvc.perform(requestBuilder)
       .andDo(print())
       .andExpect(status().isOk())
-      .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(Matchers.not(0)));
+      .andExpect(jsonPath("$.length()")
+        .value(Matchers.is(2)))
+      .andExpect(jsonPath("$[0].number").value(data[1].getNumber()))
+      .andExpect(jsonPath("$[0]['html_url']").value(data[1].getHtmlUrl()))
+      .andExpect(jsonPath("$[0]['title']").value(data[1].getTitle()))
+      .andExpect(jsonPath("$[0]['merged_at']").value(data[1].getMergedAt().toString()))
+      .andExpect(jsonPath("$[0]['user_type']").value(data[1].getUserType()));
 
   }
 
+  @Test
+  public void getPRs_Valid_returnsServiceUnavailable() throws Exception {
+
+    when(client.exchange(contains(url), eq(HttpMethod.GET), any(), eq(PullRequest[].class)))
+      .thenThrow(new RestClientException("Some error"));
+
+    MockHttpServletRequestBuilder requestBuilder = get(path);
+    requestBuilder.param(NewsController.STATE_PARAM, "closed");
+    requestBuilder.param(NewsController.PAGE_PARAM, "1");
+    requestBuilder.param(NewsController.PER_PAGE_PARAM, "10");
+
+    this.mockMvc.perform(requestBuilder)
+      .andDo(print())
+      .andExpect(status().isServiceUnavailable())
+      .andExpect(status().reason(ErrorMessages.serverLimitReachedMessage));
+  }
 }
