9c0e99ee8c6045ffe11ec5776a42f99ec078236c
Strittmatter, Stephan <stephan@st-strittmatter.name>
2023-10-25 15:57:10 +0200

    chore: add collapse for mobile

--- frontend/src/components/navigation/TopNavigationBar.vue
+++ frontend/src/components/navigation/TopNavigationBar.vue
@@ -4,17 +4,21 @@
       <b-img src="/img/icons/logo.svg" class="top-navigation__nav-logo" />
       {{ $t("page.landing.productTitle") }}
     </b-navbar-brand>
-    <b-navbar-nav class="ml-auto">
-      <b-button :to="{ name: 'PrepareSessionPage' }" class="startButton mr-2">New Session</b-button>
-      <b-button :to="{ name: 'JoinPage' }" class="joinButton mr-2">
-        {{ $t("page.landing.meeting.join.buttons.start.label") }}
-      </b-button>
-      <a href="https://github.com/Sybit-Education/Diveni" target="_blank">
-        <BIconGithub class="github-logo p-2"></BIconGithub>
-      </a>
-      <theme-toggle-component class="p-2" />
-      <locale-dropdown />
-    </b-navbar-nav>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-button :to="{ name: 'PrepareSessionPage' }" class="startButton mr-2">New Session</b-button>
+        <b-button :to="{ name: 'JoinPage' }" class="joinButton mr-2">
+          {{ $t("page.landing.meeting.join.buttons.start.label") }}
+        </b-button>
+        <a href="https://github.com/Sybit-Education/Diveni" target="_blank">
+          <BIconGithub class="github-logo p-2"></BIconGithub>
+        </a>
+        <theme-toggle-component class="p-2" />
+        <locale-dropdown />
+      </b-navbar-nav>
+    </b-collapse>
   </b-navbar>
 </template>
 
