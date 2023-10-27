41cc4e316fbc53767f54263c59df2905cf0421d6
G1t-Happens <44774729+G1t-Happens@users.noreply.github.com>
2023-10-27 19:12:58 +0200

    fix: let DatabaseService handle everything + introduce logging

--- backend/src/main/java/io/diveni/backend/service/DatabaseService.java
+++ backend/src/main/java/io/diveni/backend/service/DatabaseService.java
@@ -13,6 +13,8 @@ import java.util.UUID;
 import io.diveni.backend.model.Session;
 import io.diveni.backend.model.Statistic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,7 +24,11 @@ import io.diveni.backend.repository.StatisticRepository;
 @Service
 public class DatabaseService {
 
-  private final String stat_V1 = "STAT_V1";
+  private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseService.class);
+
+  //Sets the database entry identifier in our 'statistics' table.
+  //Only modify if you want to create a new database entry!!!
+  private final String statEntryIdentifier = "STAT_V1";
 
   @Autowired SessionRepository sessionRepo;
 
@@ -30,48 +36,59 @@ public class DatabaseService {
 
 
   public Optional<Session> getSessionByID(String sessionID) {
+    LOGGER.debug("getSessionByID()");
     return Optional.ofNullable(sessionRepo.findBySessionID(sessionID));
   }
 
   public Optional<Session> getSessionByAdminCookie(UUID adminCookie) {
+    LOGGER.debug("getSessionByAdminCookie()");
     return Optional.ofNullable(sessionRepo.findByAdminCookie(adminCookie));
   }
 
   public Optional<Session> getSessionByMemberID(String memberID) {
+    LOGGER.debug("getSessionByMemberID()");
     return sessionRepo.findByMemberID(memberID);
   }
 
   public List<Session> getSessions() {
+    LOGGER.debug("getSessions()");
     return sessionRepo.findAll();
   }
 
   public Session saveSession(Session session) {
+    LOGGER.debug("saveSession()");
     return sessionRepo.save(session.setLastModified(new Date()));
   }
 
   public void deleteSession(Session session) {
+    LOGGER.debug("--> deleteSession()");
     statisticRepository.save(getOrCreateStatistic().incrementOverallSessions());
     sessionRepo.delete(session);
+    LOGGER.debug("<-- deleteSession()");
   }
 
   public void addRemovedMember() {
+    LOGGER.debug("addRemovedMember()");
     statisticRepository.save(getOrCreateStatistic().incrementOverallAttendees());
   }
 
   public Integer getRemovedMember() {
-    return statisticRepository.findById(stat_V1)
+    LOGGER.debug("getRemovedMember()");
+    return statisticRepository.findById(statEntryIdentifier)
       .map(Statistic::getOverallAttendees)
       .orElse(0);
   }
 
   public Integer getDeletedSessions() {
-    return statisticRepository.findById(stat_V1)
+    LOGGER.debug("getDeletedSessions()");
+    return statisticRepository.findById(statEntryIdentifier)
       .map(Statistic::getOverallSessions)
       .orElse(0);
   }
 
   private Statistic getOrCreateStatistic() {
-    return statisticRepository.findById(stat_V1)
-      .orElseGet(() -> statisticRepository.save(new Statistic()));
+    LOGGER.debug("getOrCreateStatistic()");
+    return statisticRepository.findById(statEntryIdentifier)
+      .orElseGet(() -> statisticRepository.save(new Statistic(statEntryIdentifier, 0, 0)));
   }
 }
