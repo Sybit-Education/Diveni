8825b051afdfe96c54e41036e48b966c720a782c
G1t-Happens <44774729+G1t-Happens@users.noreply.github.com>
2023-11-04 01:29:12 +0100

    fix: make the cards slightly darker while dark-mode active and apply a text-shadow for better readability

--- frontend/src/components/CardSetComponent.vue
+++ frontend/src/components/CardSetComponent.vue
@@ -283,31 +283,47 @@ export default Vue.extend({
 .light5 {
   background-image: url("@/assets/preparePage/OwnSet-LightMode.png");
   background-size: cover;
+  filter: hue-rotate(20deg);
 }
 
 .dark1 {
   background-image: url("@/assets/preparePage/Fibonacci-DarkMode.png");
   background-size: cover;
+  filter: hue-rotate(20deg);
+  text-shadow: #000 0 0 5px;
+  -webkit-font-smoothing: antialiased;
 }
 
 .dark2 {
   background-image: url("@/assets/preparePage/TShirt-DarkMode.png");
   background-size: cover;
+  filter: hue-rotate(20deg);
+  text-shadow: #000 0 0 5px;
+  -webkit-font-smoothing: antialiased;
 }
 
 .dark3 {
   background-image: url("@/assets/preparePage/Hour-DarkMode.png");
   background-size: cover;
+  filter: hue-rotate(20deg);
+  text-shadow: #000 0 0 5px;
+  -webkit-font-smoothing: antialiased;
 }
 
 .dark4 {
   background-image: url("@/assets/preparePage/Numbers-DarkMode.png");
   background-size: cover;
+  filter: hue-rotate(20deg);
+  text-shadow: #000 0 0 5px;
+  -webkit-font-smoothing: antialiased;
 }
 
 .dark5 {
   background-image: url("@/assets/preparePage/OwnSet-DarkMode.png");
   background-size: cover;
+  filter: hue-rotate(20deg);
+  text-shadow: #000 0 0 5px;
+  -webkit-font-smoothing: antialiased;
 }
 
 #popUp {
