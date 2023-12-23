2ffa7e98e3d0f9df022e21f4f60303bad61f4acd
G1t-Happens <44774729+G1t-Happens@users.noreply.github.com>
2023-12-24 00:35:23 +0100

    fix: re-implemented with eslint-disable

--- frontend/src/shims-vue.d.ts
+++ frontend/src/shims-vue.d.ts
@@ -1,5 +1,15 @@
+/* eslint-disable */
 declare module "*.vue" {
   import Vue from "vue";
 
   export default Vue;
+}
+
+declare module "vue" {
+  import { CompatVue } from "@vue/runtime-dom";
+  const Vue: CompatVue;
+  export default Vue;
+  export * from "@vue/runtime-dom";
+  const { configureCompat } = Vue;
+  export { configureCompat };
 }
