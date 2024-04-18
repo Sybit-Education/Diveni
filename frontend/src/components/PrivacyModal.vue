4c2f41103b028273e3cf50ef31a454a483cf054e
SponsoredByPuma <tillwanner01@gmail.com>
2024-04-18 13:32:45 +0200

    improve modal design, functionality

--- frontend/src/components/PrivacyModal.vue
+++ frontend/src/components/PrivacyModal.vue
@@ -6,13 +6,14 @@
       v-model="showModal"
       centered
       hide-header-close
+      @hide="hideModal"
     >
       <template #modal-header>
         <div class="text-center">
           {{ t("general.aiFeature.privacyModal.title") }}
         </div>
       </template>
-      <BIconExclamationTriangle/>{{ t("general.aiFeature.privacyModal.warning") }}<BIconExclamationTriangle/>: {{ t("general.aiFeature.privacyModal.warningInfo") }}
+      <div id="warningText"> <BIconExclamationTriangle/> {{ t("general.aiFeature.privacyModal.warning") }}: {{ t("general.aiFeature.privacyModal.warningInfo") }}</div>
       <div>
         <b-form-textarea
           v-if="currentTitle !== ''"
@@ -23,78 +24,117 @@
         <UiToastEditorWrapper
           :initial-value="currentText"
           :none-clickable="true"
-          :class="isCompany || isNumber || isPerson ? 'confidential-modal' : ''"
         />
         <div class="d-flex justify-content-between">
           <b-button
             class="toggle-button"
-            :class="isCompany ? 'activeCompany' : ''"
-            @click="markButtonClicked('company'); $event.target.blur()"
+            @click="addWordToList('company'); $event.target.blur()"
           >
             <BIconHouseDoorFill/> {{ t("general.aiFeature.privacyModal.buttons.company") }}
           </b-button>
           <b-button
             class="toggle-button"
-            :class="isPerson ? 'activePerson' : ''"
-            @click="markButtonClicked('person')"
+            @click="addWordToList('person'); $event.target.blur()"
           >
             <BIconPersonFill/> {{ t("general.aiFeature.privacyModal.buttons.person") }}
           </b-button>
           <b-button
             class="toggle-button"
-            :class="isNumber ? 'activeNumber' : ''"
-            @click="markButtonClicked('number')"
+            @click="addWordToList('number'); $event.target.blur()"
           >
             <BIconCalculatorFill/> {{ t("general.aiFeature.privacyModal.buttons.number") }}
           </b-button>
         </div>
-        <div class="mt-2">
-          <b-button
-            id="addWordsToListButton"
-            @click="addWordToList()"
-          >
-            <BIconPlus/> {{ t("general.aiFeature.privacyModal.buttons.add") }}
-          </b-button>
-        </div>
         <div
-          class="text-center"
+          class="d-flex justify-content-between mt-3"
         >
-          <div
-            v-for="(data, index) in confidentialWords.keys()"
-            :key="data"
-            class="d-inline-block"
-          >
+          <div class="confidential-words-div">
             <div
-              id="confidential-words"
-              class="mx-2"
-              @mouseover="hover = index"
-              @mouseleave="hover = null"
+              v-for="(data, index) in confidentialWords.keys()"
+              :key="data"
             >
-              <BIconHouseFill v-if="confidentialWords.get(data) === 'company'"/>
-              <BIconPersonFill v-if="confidentialWords.get(data) === 'person'"/>
-              <BIconCalculatorFill v-if="confidentialWords.get(data) === 'number'"/>
-              {{data}}
-
-              <b-button
-                variant="outline-danger"
-                class="border-0"
-                size="sm"
-                @click="confidentialWords.delete(data)"
+              <div
+                v-if="confidentialWords.get(data) === 'company'"
+                @mouseover="hover = index"
+                @mouseleave="hover = null"
               >
-                <b-icon-trash />
-              </b-button>
+                <BIconHouseFill/>
+                {{data}}
+
+                <b-button
+                  variant="outline-danger"
+                  class="border-0"
+                  size="sm"
+                  @click="confidentialWords.delete(data);
+                caseSensitiveList = caseSensitiveList.filter(word => word !== data.toLowerCase());"
+                >
+                  <b-icon-trash />
+                </b-button>
+              </div>
+            </div>
+          </div>
+          <div class="confidential-words-div">
+            <div
+              v-for="(data, index) in confidentialWords.keys()"
+              :key="data"
+            >
+              <div
+                v-if="confidentialWords.get(data) === 'person'"
+                class="mx-2"
+                @mouseover="hover = index"
+                @mouseleave="hover = null"
+              >
+                <BIconPersonFill/>
+                {{data}}
+
+                <b-button
+                  variant="outline-danger"
+                  class="border-0"
+                  size="sm"
+                  @click="confidentialWords.delete(data);
+                caseSensitiveList = caseSensitiveList.filter(word => word !== data.toLowerCase());"
+                >
+                  <b-icon-trash />
+                </b-button>
+              </div>
+            </div>
+          </div>
+          <div class="confidential-words-div">
+            <div
+              v-for="(data, index) in confidentialWords.keys()"
+              :key="data"
+            >
+              <div
+                v-if="confidentialWords.get(data) === 'number'"
+                class="mx-2"
+                @mouseover="hover = index"
+                @mouseleave="hover = null"
+              >
+                <BIconCalculatorFill/>
+                {{data}}
+
+                <b-button
+                  variant="outline-danger"
+                  class="border-0"
+                  size="sm"
+                  @click="confidentialWords.delete(data);
+                caseSensitiveList = caseSensitiveList.filter(word => word !== data.toLowerCase());"
+                >
+                  <b-icon-trash />
+                </b-button>
+              </div>
             </div>
           </div>
         </div>
       </div>
       <div v-if="isDescription" class="my-2 d-inline-flex">
         <div class="position-relative mb-2">
-          <input class="position-absolute" style="height: 20px; width:20px; left: 150px; top: 2px;" v-model="selectedLanguage" type="radio" name="languageSelector" value="english">
-          <label for="english" class="position-absolute" style="left: 175px;">{{ t("general.aiFeature.privacyModal.labels.english") }}</label>
+          <input id="english-radio-button" class="position-absolute" v-model="selectedLanguage" type="radio" name="languageSelector" value="english">
+          <label for="english" id="english-label" class="position-absolute">{{ t("general.aiFeature.privacyModal.labels.english") }}</label>
         </div>
         <div class="position-relative mb-2">
-          <input class="position-absolute" style="height: 20px; width:20px; left: 250px; top: 2px;" v-model="selectedLanguage" type="radio" name="languageSelector" value="german">
-          <label for="german" style="left: 275px" class="position-absolute">{{ t("general.aiFeature.privacyModal.labels.german") }}</label>
+          <input id="german-radio-button" class="position-absolute" v-model="selectedLanguage" type="radio" name="languageSelector" value="german">
+          <label for="german" id="german-label" class="position-absolute">{{ t("general.aiFeature.privacyModal.labels.german") }}</label>
         </div>
       </div>
       <template #modal-footer >
@@ -134,10 +174,8 @@ export default defineComponent({
   data() {
     return {
       confidentialWords: {} as Map<string, string>,
+      caseSensitiveList: [] as Array<string>,
       hover: null,
-      isCompany: false,
-      isPerson: false,
-      isNumber: false,
       showModal: true,
       selectedLanguage: "english",
       currentTitleData: "",
@@ -148,40 +186,12 @@ export default defineComponent({
     this.currentTitleData = this.currentTitle;
   },
   methods: {
-    markButtonClicked(type) {
-      switch (type) {
-        case 'company':
-          this.isCompany = !this.isCompany;
-          this.isPerson = false;
-          this.isNumber = false;
-          document.documentElement.style.setProperty("--markConfidentialInformation", "red");
-          break;
-        case 'person':
-          this.isPerson = !this.isPerson;
-          this.isCompany = false;
-          this.isNumber = false;
-          document.documentElement.style.setProperty("--markConfidentialInformation", "green");
-          break;
-        case 'number':
-          this.isNumber = !this.isNumber;
-          this.isCompany = false;
-          this.isPerson = false;
-          document.documentElement.style.setProperty("--markConfidentialInformation", "#053C5E");
-          break;
-      }
-    },
-    addWordToList() {
-      const selection = window.getSelection()
-      if (selection !== null && selection.toString() !== '' && !this.confidentialWords.has(selection.toString())) {
-        if (this.isCompany) {
-          this.confidentialWords.set(selection.toString(), "company");
-        }
-        if (this.isPerson) {
-          this.confidentialWords.set(selection.toString(), "person");
-        }
-        if (this.isNumber) {
-          this.confidentialWords.set(selection.toString(), "number");
-        }
+    addWordToList(type: string) {
+      const selection = window.getSelection();
+      if (selection !== null && selection.toString().trim() !== '' && !this.caseSensitiveList.includes(selection.toString().trim().toLowerCase())
+        && (this.currentText.includes(selection.toString().trim()) || this.currentTitle.includes(selection.toString().trim()))) {
+        this.confidentialWords.set(selection.toString().trim(), type);
+        this.caseSensitiveList.push(selection.toString().trim().toLowerCase())
       }
     },
     submitIssue() {
@@ -190,7 +200,6 @@ export default defineComponent({
       } else {
         this.$emit("sendGPTRequest", {confidentialData: this.confidentialWords });
       }
-
       this.hideModal();
     },
     hideModal() {
@@ -201,51 +210,41 @@ export default defineComponent({
 })
 </script>
 <style lang="scss">
+.confidential-words-div {
+  display: block;
+  width: 33%;
+}
+
+#warningText {
+  font-style: italic;
+}
+
 #titleInputField {
   border: 0;
   background-color: var(--text-field) !important;
   color: var(--text-primary-color);
 }
 
-.confidential-modal {
-  .toastui-editor-contents p::selection {
-    color: var(--text-primary-color);
-    background-color: var(--markConfidentialInformation);
-    font-size: large;
-  }
-}
-
 #addWordsToListButton {
   width: 100%;
 }
 
-.toggle-button {
-  background-color: #0074d9 !important;
-  color: #ffffff !important;
-  border: none;
-  cursor: pointer;
-  transition: transform 0.2s ease;
+#english-radio-button {
+  height: 20px;
+  width:20px;
+  left: 150px;
+  top: 2px;
 }
-
-.toggle-button:focus {
-  color: white;
+#english-label {
+  left: 175px;
 }
-
-.toggle-button.activeCompany {
-  transform: translateY(5px);
-  background-color: red !important;
-  /* Add any other styles for the "in use" state */
+#german-radio-button {
+  height: 20px;
+  width:20px;
+  left: 250px;
+  top: 2px;
 }
-
-.toggle-button.activePerson {
-  transform: translateY(5px);
-  background-color: green !important;
-  /* Add any other styles for the "in use" state */
-}
-
-.toggle-button.activeNumber {
-  transform: translateY(5px);
-  background-color: #053C5E !important;
-  /* Add any other styles for the "in use" state */
+#german-label {
+  left: 275px
 }
 </style>
