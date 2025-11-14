e99f713573c58bc7a0bdc7dd2af49a4c9fc3e5cb
Daniel <Daniel.Boxheimer@sybit.de>
2025-11-14 15:28:30 +0100

    test

--- backend/src/main/java/io/diveni/backend/controller/NewsController.java
+++ backend/src/main/java/io/diveni/backend/controller/NewsController.java
@@ -14,10 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class NewsController {
 
   public static final String STATE_PARAM = "state";
-  public static final String PAGE_PARAM = "page";
+    public static final String PAGE_PARAM = "page";
   public static final String PER_PAGE_PARAM = "per_page";
   public static final String IS_MERGED = "is_merged";
-  public static final String SORT = "sort";
+    public static final String SORT = "sort";
   public static final String SORT_DIRECTION = "direction";
 
   private GithubApiService service;
@@ -36,7 +36,7 @@ public class NewsController {
       @RequestParam(name = PER_PAGE_PARAM, defaultValue = "50") Integer perPage,
       @RequestParam(name = PAGE_PARAM, defaultValue = "1") Integer page) {
 
-    return new ResponseEntity<>(
+      return new ResponseEntity<>(
         service.getPullRequests(state, sort, direction, isMerged, perPage, page), HttpStatus.OK);
   }
 }
