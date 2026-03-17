da68ca7f51b9730f48061c8c13370587711671fb
G1t-Happens <44774729+G1t-Happens@users.noreply.github.com>
2026-03-17 23:52:36 +0100

    fix: warnings

--- frontend/src/shims-vue.d.ts
+++ frontend/src/shims-vue.d.ts
@@ -1,10 +1,15 @@
+/* eslint-disable @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any */
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
 declare module "vue" {
   import { CompatVue } from "@vue/runtime-dom";
   const Vue: CompatVue;
   export default Vue;
   export * from "@vue/runtime-dom";
-  const { configureCompat } = Vue;
-  export { configureCompat };
 }
 
 declare module "vue3-steppy";
